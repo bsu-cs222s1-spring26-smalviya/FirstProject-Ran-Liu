e31f1f420107400e7ec4d9ae7bd4b3b27c3f5a84
KyrionRan <kyrionran@outlook.com>
2026-02-09 18:19:17 -0500

    Add Revision Class

---
+++ src/main/java/edu/bsu/cs/Revision.java
@@ -0,0 +1,19 @@
+package edu.bsu.cs;
+
+public class Revision {
+    private String user;
+    private String timestamp;
+
+    public Revision(String user, String timestamp) {
+        this.user = user;
+        this.timestamp = timestamp;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getTimeStamp() {
+        return timestamp;
+    }
+}
---
+++ src/test/java/edu/bsu/cs/RevisionTest.java
@@ -0,0 +1,18 @@
+package edu.bsu.cs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RevisionTest {
+    Revision revision = new Revision("Test", "2026-02-08T20:00:22Z");
+
+    @Test
+    public void getUser() {
+        Assertions.assertEquals("Test", revision.getUser());
+    }
+
+    @Test
+    public void getTimeStamp() {
+        Assertions.assertEquals("2026-02-08T20:00:22Z", revision.getTimeStamp());
+    }
+}
