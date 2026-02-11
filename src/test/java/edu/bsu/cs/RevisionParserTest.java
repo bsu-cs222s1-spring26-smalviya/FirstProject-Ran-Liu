d169ceffe61abcb81fb6daf0603cac3d938233ca
KyrionRan <kyrionran@outlook.com>
2026-02-11 14:11:21 -0500

    Change variable names in RevisionParserTest

--- src/test/java/edu/bsu/cs/RevisionParserTest.java
+++ src/test/java/edu/bsu/cs/RevisionParserTest.java
@@ -9,8 +9,8 @@ import java.nio.charset.Charset;
 import java.util.Objects;
 
 public class RevisionParserTest {
-    RevisionParser revisionParser;
-    RevisionParser revisionParser2;
+    RevisionParser revisionParser_Redirect;
+    RevisionParser revisionParser_NoRedirect;
 
     public RevisionParserTest() throws NullPointerException, IOException {
         try (InputStream jsonFile = Thread.currentThread().getContextClassLoader()
@@ -19,7 +19,7 @@ public class RevisionParserTest {
                 throw new IllegalStateException("[Error] JSON file not found! ");
             }
             String jsonData = new String(Objects.requireNonNull(jsonFile).readAllBytes(), Charset.defaultCharset());
-            revisionParser = new RevisionParser(jsonData);
+            revisionParser_Redirect = new RevisionParser(jsonData);
         }
         try (InputStream jsonFile = Thread.currentThread().getContextClassLoader()
                 .getResourceAsStream("Case_NoRedirect.json")) {
@@ -27,53 +27,53 @@ public class RevisionParserTest {
                 throw new IllegalStateException("[Error] JSON file not found! ");
             }
             String jsonData = new String(Objects.requireNonNull(jsonFile).readAllBytes(), Charset.defaultCharset());
-            revisionParser2 = new RevisionParser(jsonData);
+            revisionParser_NoRedirect = new RevisionParser(jsonData);
         }
     }
 
     @Test
     public void revisionsCount() {
-        Assertions.assertEquals(4, revisionParser.getRevisionsCount());
-        Assertions.assertEquals(4, revisionParser2.getRevisionsCount());
+        Assertions.assertEquals(4, revisionParser_Redirect.getRevisionsCount());
+        Assertions.assertEquals(4, revisionParser_NoRedirect.getRevisionsCount());
     }
 
     @Test
     public void revisionsUser() {
-        Assertions.assertEquals("Smasongarrison", revisionParser.getRevisionsUser(0));
-        Assertions.assertEquals("Smasongarrison", revisionParser.getRevisionsUser(1));
-        Assertions.assertEquals("Citation bot", revisionParser.getRevisionsUser(2));
-        Assertions.assertEquals("Smasongarrison", revisionParser.getRevisionsUser(3));
-        Assertions.assertEquals("Smasongarrison", revisionParser2.getRevisionsUser(0));
-        Assertions.assertEquals("Smasongarrison", revisionParser2.getRevisionsUser(1));
-        Assertions.assertEquals("Citation bot", revisionParser2.getRevisionsUser(2));
-        Assertions.assertEquals("Smasongarrison", revisionParser2.getRevisionsUser(3));
+        Assertions.assertEquals("Smasongarrison", revisionParser_Redirect.getRevisionsUser(0));
+        Assertions.assertEquals("Smasongarrison", revisionParser_Redirect.getRevisionsUser(1));
+        Assertions.assertEquals("Citation bot", revisionParser_Redirect.getRevisionsUser(2));
+        Assertions.assertEquals("Smasongarrison", revisionParser_Redirect.getRevisionsUser(3));
+        Assertions.assertEquals("Smasongarrison", revisionParser_NoRedirect.getRevisionsUser(0));
+        Assertions.assertEquals("Smasongarrison", revisionParser_NoRedirect.getRevisionsUser(1));
+        Assertions.assertEquals("Citation bot", revisionParser_NoRedirect.getRevisionsUser(2));
+        Assertions.assertEquals("Smasongarrison", revisionParser_NoRedirect.getRevisionsUser(3));
     }
 
     @Test
     public void revisionTime() {
-        Assertions.assertEquals("2026-01-25T23:51:24Z", revisionParser.getRevisionsTime(0));
-        Assertions.assertEquals("2026-01-25T23:26:54Z", revisionParser.getRevisionsTime(1));
-        Assertions.assertEquals("2026-01-21T20:29:46Z", revisionParser.getRevisionsTime(2));
-        Assertions.assertEquals("2026-01-20T05:29:08Z", revisionParser.getRevisionsTime(3));
-        Assertions.assertEquals("2026-01-25T23:51:24Z", revisionParser2.getRevisionsTime(0));
-        Assertions.assertEquals("2026-01-25T23:26:54Z", revisionParser2.getRevisionsTime(1));
-        Assertions.assertEquals("2026-01-21T20:29:46Z", revisionParser2.getRevisionsTime(2));
-        Assertions.assertEquals("2026-01-20T05:29:08Z", revisionParser2.getRevisionsTime(3));
+        Assertions.assertEquals("2026-01-25T23:51:24Z", revisionParser_Redirect.getRevisionsTime(0));
+        Assertions.assertEquals("2026-01-25T23:26:54Z", revisionParser_Redirect.getRevisionsTime(1));
+        Assertions.assertEquals("2026-01-21T20:29:46Z", revisionParser_Redirect.getRevisionsTime(2));
+        Assertions.assertEquals("2026-01-20T05:29:08Z", revisionParser_Redirect.getRevisionsTime(3));
+        Assertions.assertEquals("2026-01-25T23:51:24Z", revisionParser_NoRedirect.getRevisionsTime(0));
+        Assertions.assertEquals("2026-01-25T23:26:54Z", revisionParser_NoRedirect.getRevisionsTime(1));
+        Assertions.assertEquals("2026-01-21T20:29:46Z", revisionParser_NoRedirect.getRevisionsTime(2));
+        Assertions.assertEquals("2026-01-20T05:29:08Z", revisionParser_NoRedirect.getRevisionsTime(3));
     }
 
     @Test
     public void isRedirect() {
-        Assertions.assertTrue(revisionParser.isRevisionsRedirects());
-        Assertions.assertFalse(revisionParser2.isRevisionsRedirects());
+        Assertions.assertTrue(revisionParser_Redirect.isRevisionsRedirects());
+        Assertions.assertFalse(revisionParser_NoRedirect.isRevisionsRedirects());
     }
 
     @Test
     public void revisionRedirectFrom() {
-        Assertions.assertEquals("John von Neuman", revisionParser.getRevisionsRedirectFrom());
+        Assertions.assertEquals("John von Neuman", revisionParser_Redirect.getRevisionsRedirectFrom());
     }
 
     @Test
     public void revisionRedirectTo() {
-        Assertions.assertEquals("John von Neumann", revisionParser.getRevisionsRedirectTo());
+        Assertions.assertEquals("John von Neumann", revisionParser_Redirect.getRevisionsRedirectTo());
     }
 }
