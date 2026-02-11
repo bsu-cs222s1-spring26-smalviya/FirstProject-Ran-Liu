697cdd7999219eff5cf0cab3656804817f55640d
KyrionRan <kyrionran@outlook.com>
2026-02-11 16:02:32 -0500

    Feat: Add error handling for invalid input
    - Add a check for invalid input in ConsoleMenu
    - Print a red system warning and exit the program when invalid input

--- src/main/java/edu/bsu/cs/ConsoleMenu.java
+++ src/main/java/edu/bsu/cs/ConsoleMenu.java
@@ -12,6 +12,10 @@ public class ConsoleMenu {
         System.out.println("Wikipedia Revision Reporter");
         System.out.print("Search a title to begin: ");
         String title = scanner.nextLine();
+        if(title.isBlank()){
+            System.err.println("[Warning] Invalid input!");
+            System.exit(1);
+        }
         wikipediaService.searchTitle(title);
         System.out.print(wikipediaService.getRevisionTable());
     }
