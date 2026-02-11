ab248d46aaf2dbb2b33a80597b606b6777a0e082
Yixiao Liu <tljsjdr@gmail.com>
2026-02-11 13:36:06 -0500

    Rename Menu to ConsoleMenu

--- src/main/java/edu/bsu/cs/ConsoleMenu.java
+++ src/main/java/edu/bsu/cs/ConsoleMenu.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Scanner;
 
-public class Menu {
+public class ConsoleMenu {
     WikipediaService wikipediaService = new WikipediaService();
     Scanner scanner = new Scanner(System.in);
 
-    public void showMenu() throws IOException, URISyntaxException {
+    public void showConsoleMenu() throws IOException, URISyntaxException {
         System.out.println("Wikipedia Revision Reporter");
         System.out.print("Search a title to begin: ");
         String title = scanner.nextLine();
