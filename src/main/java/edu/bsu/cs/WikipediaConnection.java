ef01e75fe1c76ead8f519b1e781f4dff69877e66
KyrionRan <kyrionran@outlook.com>
2026-02-11 16:14:11 -0500

    Feat: Add error handling for network error
    - Add catch for network error in WikipediaConnection
    - Print a red system warning and exit the program when network error

--- src/main/java/edu/bsu/cs/WikipediaConnection.java
+++ src/main/java/edu/bsu/cs/WikipediaConnection.java
@@ -11,13 +11,17 @@ public class WikipediaConnection {
     URLConnection connection;
 
     public void connectWikipedia(String title) throws IOException, URISyntaxException {
-        String encodedUrlString = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" +
-                URLEncoder.encode(title, Charset.defaultCharset()) +
-                "&rvprop=timestamp" + URLEncoder.encode("|", Charset.defaultCharset()) + "user&rvlimit=15&redirects";
-        URI uri = new URI(encodedUrlString);
-        connection = uri.toURL().openConnection();
-        connection.setRequestProperty("User-Agent", "Wikipedia Revision Reporter/0.1 (qingyang.ran@bsu.edu)");
-        connection.connect();
+        try {
+            String encodedUrlString = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" +
+                    URLEncoder.encode(title, Charset.defaultCharset()) +
+                    "&rvprop=timestamp" + URLEncoder.encode("|", Charset.defaultCharset()) + "user&rvlimit=15&redirects";
+            URI uri = new URI(encodedUrlString);
+            connection = uri.toURL().openConnection();
+            connection.setRequestProperty("User-Agent", "Wikipedia Revision Reporter/0.1 (qingyang.ran@bsu.edu)");
+            connection.connect();
+        } catch (IOException e) {
+            System.err.println("[Error] Network connection failed, please check your network status!");
+        }
     }
 
     public String getJsonAsString() throws IOException {
